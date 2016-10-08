73b44098a36c697fcb733b0df0c76ed9c3f8c806
nayf <nayfania@gmail.com>
2016-10-08 23:32:53 +0300

    init codeception project

---
+++ tests/acceptance/PicturesCest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class PicturesCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->amOnPage('/pictures-to-gif');
+        $I->see('Pictures to GIF');
+    }
+}
