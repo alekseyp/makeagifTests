5659ba6c89a4897476c57ff6f1d79cc3e0f88925
nayf <nayfania@gmail.com>
2016-10-09 23:35:07 +0300

    Pictures - Unregistered - Create suite

--- tests/acceptance/PicturesCest.php
+++ tests/acceptance/PicturesCest.php
@@ -1,20 +1,43 @@
 <?php
 
-
+/**
+ * Class PicturesCest
+ */
 class PicturesCest
 {
-    public function _before(AcceptanceTester $I)
-    {
-    }
-
-    public function _after(AcceptanceTester $I)
-    {
-    }
-
-    // tests
-    public function tryToTest(AcceptanceTester $I)
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function unregistered(AcceptanceTester $I)
     {
         $I->amOnPage('/pictures-to-gif');
         $I->see('Pictures to GIF');
+
+        // Upload 3 images
+        $I->attachFile('#Filedata', 'image1.jpg');
+        $I->attachFile('#Filedata', 'image2.jpg');
+        $I->attachFile('#Filedata', 'image3.jpg');
+        // Check pictures preview
+        $I->seeNumberOfElements('#thumbnails .card', 3);
+
+        $I->click('Continue');
+
+        $I->fillField('title', 'Beautiful');
+        $I->fillField(['id' => 'tags_text'], 'beautiful b1');
+        // @todo need to check it after create
+        $I->selectOption('select#delay_select', 'slower 1000ms delay');
+        $I->selectOption('select#resize', 'To smallest cropped');
+
+        $I->click('#create');
+
+        $I->waitForElement('.vex.vex-theme-plain');
+        $I->waitForElementNotVisible('.vex.vex-theme-plain', 30);
+
+        $I->wait(1);
+        $I->waitForJS('return document.readyState=="complete"');
+
+        $I->see('Beautiful', 'h1');
+        $I->see('#beautiful', '.tags');
+        $I->see('#b1', '.tags');
     }
 }
