fb84c54d64ff3e3a0fb64b900c42b9624fb6fd42
nayf <nayfania@gmail.com>
2016-10-12 22:37:23 +0300

    Add Authorized pictures to gif cest; Add Acceptance Helper for login

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -6,5 +6,25 @@ namespace Helper;
 
 class Acceptance extends \Codeception\Module
 {
+    /**
+     * @param \AcceptanceTester $I
+     * @param $email
+     * @param $password
+     */
+    public function login(\AcceptanceTester $I, $email, $password)
+    {
+        $I->click('Log in', '.topbar__user');
+        $I->waitForElementVisible('.vex-content');
 
+        $I->fillField('logusername', $email);
+        $I->fillField('logpassword', $password);
+//        $I->click(['class' => 'label', 'for' => 'remember']);
+        $I->executeJS('$(".vex-overlay").hide();');
+        $I->click('form#login .button._color_yellow');
+        $I->waitForElementNotVisible('.vex-content');
+
+        $I->reloadPage();
+
+        $I->see('nayf', '.user-info');
+    }
 }
--- tests/acceptance/PicturesCest.php
+++ tests/acceptance/PicturesCest.php
@@ -23,7 +23,54 @@ class PicturesCest
         $I->click('Continue');
 
         $I->fillField('title', 'Beautiful');
-        $I->fillField(['id' => 'tags_text'], 'beautiful b1');
+        $I->fillField(['id' => 'tags_text'], 'beautiful b1 ');
+        // @todo need to check it after create
+        $I->selectOption('select#delay_select', 'slower 1000ms delay');
+        $I->selectOption('select#resize', 'To smallest cropped');
+
+        $I->click('#create');
+
+        $I->waitForElement('.vex.vex-theme-plain');
+        $I->waitForElementNotVisible('.vex.vex-theme-plain', 30);
+
+        $I->wait(1);
+        $I->waitForJS('return document.readyState=="complete"');
+
+        $I->see('Beautiful', 'h1');
+        $I->see('#beautiful', '.tags');
+        $I->see('#b1', '.tags');
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function authorized(AcceptanceTester $I)
+    {
+        $I->amOnPage('/pictures-to-gif');
+        $I->see('Pictures to GIF');
+
+        /**
+         * Log in
+         */
+        $email = 'nayfania@gmail.com'; //@todo need to change in the future
+        $password = '27162000';
+        $I->login($I, $email, $password);
+
+        /**
+         * Upload pictures
+         */
+
+        // Upload 3 images
+        $I->attachFile('#Filedata', 'image1.jpg');
+        $I->attachFile('#Filedata', 'image2.jpg');
+        $I->attachFile('#Filedata', 'image3.jpg');
+        // Check pictures preview
+        $I->seeNumberOfElements('#thumbnails .card', 3);
+
+        $I->click('Continue');
+
+        $I->fillField('title', 'Beautiful');
+        $I->fillField(['id' => 'tags_text'], 'beautiful b1 ');
         // @todo need to check it after create
         $I->selectOption('select#delay_select', 'slower 1000ms delay');
         $I->selectOption('select#resize', 'To smallest cropped');
