44d32e0a071034a1c613745f560e01cc6b81280e
nayf <nayfania@gmail.com>
2016-10-12 20:56:13 +0300

    Auth: login, sign up

---
+++ tests/acceptance/AuthCest.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Class AuthCest
+ */
+class AuthCest
+{
+    /**
+     * @param AcceptanceTester $I
+     */
+    public function login(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+
+        /**
+         * Check all elements
+         */
+        $I->click('Log in', '.topbar__user');
+        $I->waitForElementVisible('.vex-content');
+        $I->see('Sign in to make a gif', 'h1');
+        $I->see('Benefits of a premium membership', 'a.button');
+        $I->see('Facebook sign in', 'a.button._color_facebook._social_login');
+        $I->see('Twitter sign in', 'a.button._color_twitter._social_login');
+        $I->seeElement('input', ['name' => 'logusername']);
+        $I->seeElement('input', ['name' => 'logpassword', 'type' => 'password']);
+        $I->see('Remember me!', 'form#login');
+        $I->see('Sign in', 'button.button');
+        $I->see('Don\'t have a free account? Join MakeaGif now', 'form#login');
+        $I->see('Forgot Password', 'form#login a.link');
+
+        /**
+         * Try to login
+         */
+        $email = 'nayfania@gmail.com'; //@todo need to change in the future
+        $password = '27162000';
+        $I->fillField('logusername', $email);
+        $I->fillField('logpassword', $password);
+        $I->click(['class' => 'label', 'for' => 'remember']);
+        $I->executeJS('$(".vex-overlay").hide();');
+//        $I->executeJS('$(".vex.vex-theme-plain").hide();'); @todo need to put down this layer
+        $I->click(['class' => 'button', 'value' => 'Sign in']);
+        $I->waitForElementNotVisible('.vex-content');
+
+        $I->reloadPage();
+
+        $I->waitForElement('.user-info', 30);
+        $I->see('nayf', '.user-info');
+    }
+
+    public function signUp(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+
+        $I->click('Sign up', '.topbar__user');
+        $I->waitForElementVisible('.vex-content');
+
+        $I->see('Sign up for MakeAGif', 'h1');
+        $I->see('Benefits of a premium membership', 'a[href="/premium"]');
+        $I->see('Facebook sign in', 'a[href="/login/facebook"]');
+        $I->see('Twitter sign in', 'a[href="/login/twitter"]');
+
+        $I->seeElement('input', ['name' => 'email']);
+        $I->seeElement('input', ['name' => 'password']);
+        $I->seeElement('input', ['name' => 'cpassword']);
+        $I->seeElement('input', ['name' => 'username']);
+        $I->seeElement('iframe', ['title' => 'recaptcha widget']);
+        $I->see('sign me up!', 'button.button');
+        $I->see('Already have an account? Login here.');
+    }
+}
